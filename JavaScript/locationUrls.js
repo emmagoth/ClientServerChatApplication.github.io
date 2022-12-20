c616ca3bf65f7e689852478b5b286182ef5d377b
emmagoth <121134683+emmagoth@users.noreply.github.com>
2022-12-21 03:40:12 +0530

    Update locationUrls.js

--- JavaScript/locationUrls.js
+++ JavaScript/locationUrls.js
@@ -1,4 +1,4 @@
-export var index_html = "http://localhost/Client-Server-Application/index.html";
-export var register_html = "http://localhost/Client-Server-Application/HTML/register.html";
-export var forgotPassword_html = "http://localhost/Client-Server-Application/HTML/forgotPassword.html";
-export var home_html = "http://localhost/Client-Server-Application/HTML/home.html";
+export var index_html = "https://emmagoth.github.io/ClientServerChatApplication.github.io/index.html";
+export var register_html = "https://emmagoth.github.io/ClientServerChatApplication.github.io/HTML/register.html";
+export var forgotPassword_html = "https://emmagoth.github.io/ClientServerChatApplication.github.io//HTML/forgotPassword.html";
+export var home_html = "https://emmagoth.github.io/ClientServerChatApplication.github.io//HTML/home.html";
